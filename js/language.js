b26541d168352d3fe82909a711ae28600565a96c
Jean Solano <jeansolano98@gmail.com>
2024-08-02 21:41:37 -0500

    Security Updated

--- js/language.js
+++ js/language.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     ConveyThis_Initializer.init({
-      api_key: process.env.CONVEY_THIS_API_KEY,
+      api_key: "pub_b65164c1a6cf3c91ec570d194dcf4f8f",
     });
   });
 // Order Products
